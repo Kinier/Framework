c19dda641666fde56867b802b21fe6020a75967d
kinier <govormaxsim@gmail.com>
2022-11-09 21:53:23 +0300

    core const

--- Fw/init.php
+++ Fw/init.php
@@ -1,8 +1,15 @@
 <?php
 
+
+use Fw\Core\Application;
+use Fw\Core\Multiton;
+
 spl_autoload_register(function ($class) {
     if (file_exists("$class" . '.php'))
         include $class . '.php';
 });
 
+const CORE_SET = true;
 session_start();
+
+Multiton::getInstance(Application::class);
