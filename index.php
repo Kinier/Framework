5ff0e1a22219e8f156a168229306333313fd1f4b
kinier <govormaxsim@gmail.com>
2022-11-09 21:53:44 +0300

    index

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
+
 require_once "./Fw/init.php";
+
+defined('CORE_SET') ?? exit();
+
+use Fw\Core\Application;
+use Fw\Core\Multiton;
+
+$app = Multiton::getInstance(Application::class);
+$app->header();
+?>
+    <pre>
+-------- 09.11.2022 --------
+1) создан класс Page для работы с содержимым html страницы
+2) итд
+-------- 04.11.2022 --------
+1) создан конфиг и класс для работы с ними
+2) создана функции авто регистрации классов
+</pre>
+<?php $app->footer(); ?>
