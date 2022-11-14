7b24384d7abdfacf4e1e3ff710cc0e166bda842c
Maxim <66032656+Kinier@users.noreply.github.com>
2022-11-14 11:08:18 +0300

    Application

--- Fw/Core/Application.php
+++ Fw/Core/Application.php
@@ -33,7 +33,19 @@ class Application{
         include __DIR__. "/../templates/" . Config::get('templateSettings/template') . '/footer.php';
         $content = ob_get_contents();
 
+        $replace = Page::getInstance()->getAllReplace();
+
+
+        foreach ($replace as $macro => $value) {
+            if (is_array($value)) {
+                $str = implode('', $value);
+                $content = str_replace($macro, $str, $content);
+            } else {
+                $content = str_replace($macro, $value, $content);
+            }
+        }
         ob_end_clean();
+        
         echo $content;
     }
 
