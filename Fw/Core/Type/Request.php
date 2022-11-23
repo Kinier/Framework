487e953655a7a5ee2cd875ad77a31eb1d6b99f8c
kinier <govormaxsim@gmail.com>
2022-11-23 20:29:38 +0300

    data class

---
+++ Fw/Core/Type/Request.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Fw\Core\Type;
+
+class Request extends Dictionary
+{
+    public function __construct()
+    {
+        parent::__construct($_REQUEST, true);
+    }
+}
---
+++ Fw/Core/Type/Session.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Fw\Core\Type;
+
+class Session extends Dictionary
+{
+    public function __construct()
+    {
+        parent::__construct($_SESSION,);
+    }
+}
