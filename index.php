6d11e983a5d202520cec1d3eca4e35408778815e
kinier <govormaxsim@gmail.com>
2022-11-23 20:30:03 +0300

    component include

--- index.php
+++ index.php
@@ -10,8 +10,22 @@ use Fw\Core\Multiton;
 
 $app = Multiton::getInstance(Application::class);
 $app->header();
+Application::includeComponent(
+    'fw:element.list',
+    'default',
+    [
+        "sort" => "id",
+        "limit" => 10,
+        "show_title" => "N"
+    ]
+);
+
 ?>
     <pre>
+        -------- 23.11.2022 --------
+1) Созданы классы для работы с данными запросов
+2) Application добавлено подключение компонента
+3) Создана структура компонентов и их шаблонов
 -------- 09.11.2022 --------
 1) создан класс Page для работы с содержимым html страницы
 2) итд
@@ -19,4 +33,5 @@ $app->header();
 1) создан конфиг и класс для работы с ними
 2) создана функции авто регистрации классов
 </pre>
-<?php $app->footer(); ?>
+<?php
+$app->footer(); ?>
